f5a1b7d22263e396e0d452e79db4b942d26de5e8
vasundra11 <95366675+vasundra11@users.noreply.github.com>
2022-11-14 13:41:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './App.css';
+import './style.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <center>
+          <h1>cartoons</h1>
+      <div class="cards">
+    <article class="card">
+        <header>
+            <h2></h2>
+        </header>    
+        <center>
+      <img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPg-Mo5It49KoaUZbpEFG1R82FVtc60hVogA&usqp=CAU"/>
+
+        </center>
+        <div class="content">
+          
+        <p>Tom and Jerry, American animated cartoon series about a hapless cat’s never-ending pursuit of a clever mouse
+        After Hanna and Barbera left MGM, the series was revived several times, most notably in the mid-1960s under the direction of famed animator Chuck Jones. These later versions changed certain elements of the series and softened the violence. The shorts became popular on television, and Hanna and Barbera’s own company acquired the rights to create new Tom and Jerry episodes specifically for the small screen, producing 48 stories between 1975 and 1977. The show remained a television staple for decades, although racist or other offensive elements from the early features were usually edited.
+</p>
+<footer>
+        <button className='button'>Learn more about cartoons</button>
+        </footer>
+        <br></br>
+        </div>
+            
+    </article>           
+   
+</div>
+            
+</center>
+<br></br>
+<br></br>
+      </header>
+    </div>
+  );
+}
+ 
+export default App;
+ 
+
